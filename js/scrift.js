b4a7d457055dbd849204bd7ee672aba4c56a9eac
lelynguyen <lelynguyen2907@gmail.com>
2022-06-02 15:53:48 +0700

    Add new project index

---
+++ js/scrift.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+    $('#menu').click(function(){
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+    });
+
+    $('#login').click(function(){
+        $('.login-form').addClass('popup');
+    });
+
+    $('.login-form form .fa-times').click(function(){
+        $('.login-form').removeClass('popup');
+    });
+
+});
